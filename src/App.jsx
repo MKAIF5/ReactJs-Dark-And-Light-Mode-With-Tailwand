2d2458913889bbc2506a93f10143ef5cd6151121
mohammed kaif <mkaif555123@gmail.com>
2024-10-18 19:23:49 +0500

    20% Work Completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,19 @@ import './App.css'
 function App() {
   return (
     <>
-      <div className='p-3 bg-white w-56 mx-auto my-52'>
+      <div className='p-4 bg-white w-96 mx-auto my-40 rounded-xl'>
+        <br />
         <div>
-          <h1>Sign Up</h1>
+          <h1 className='text-center text-purple-900 text-2xl
+          font-semibold'>Sign In</h1>
+        </div>
+        <br /><br /><br />
+        <div>
+          <input className='
+          bg-gray-200' type="text" name="" id="" />
+          <br />
+          <input className='
+          ' type="text" name="" id="" />
         </div>
 
 
