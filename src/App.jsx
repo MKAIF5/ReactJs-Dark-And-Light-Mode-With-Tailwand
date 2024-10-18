620494a79a919b85176ca31bca1acbdb585d0ea5
mohammed kaif <mkaif555123@gmail.com>
2024-10-18 19:19:35 +0500

    10% Work Completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ import './App.css'
 function App() {
   return (
     <>
+      <div className='p-3 bg-white w-56 mx-auto my-52'>
+        <div>
+          <h1>Sign Up</h1>
+        </div>
+
+
+      </div>
+
     </>
   )
 }
