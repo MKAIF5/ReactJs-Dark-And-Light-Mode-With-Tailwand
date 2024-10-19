a1aa52b0ac998cc2b1c1aa80e326dcac30ffeae3
mohammed kaif <mkaif555123@gmail.com>
2024-10-19 15:01:05 +0500

    60% Work Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,61 @@
+import { createContext } from 'react'
 import './App.css'
+import { useState } from 'react';
+
+
+export const ThemeContext = createContext(null);
+
 
 function App() {
-  return (
-    <>
-      <div className='p-4 bg-white w-96 mx-auto my-28 rounded-xl'>
-        <br />
-        <div>
-          <h1 className='text-center text-purple-900 text-2xl
-          font-semibold'>Sign In</h1>
-        </div>
-        <br /><br /><br />
-        <div>
-          <input className='
-          bg-gray-100 w-64 mx-12 outline-none p-1
-          rounded-xl text-center font-semibold placeholder-purple-600'
-            placeholder='Username' type="text" name="" id="" />
-          <br /><br />
-          <input className='
-          bg-gray-100 w-64 mx-12 outline-none p-1
-          rounded-xl placeholder-purple-600 text-center font-semibold'
-            placeholder='Password' type="text" name="" id="" />
-        </div>
-        <br /><br />
-        <div className='flex justify-center'>
-          <button className='p-2 w-28 rounded-2xl bg-purple-600
-           text-white'>Sign In</button>
-        </div>
-        <br />
-        <div>
-          <p className='text-center text-purple-500
-          '>Forgot Password?</p>
-        </div>
-        <br />
-      </div>
 
-    </>
-  )
+  const [theme, setTheme] = useState("dark")
+
+  const toggleTheme = () => {
+    setTheme((curr) => {
+      curr === "light" ? "dark" : "light"
+    });
+  };
+  
+
+return (
+  <>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+
+      <div id='light'>
+        <div className='p-4 bg-white w-96 mx-auto my-28 rounded-xl'>
+          <br />
+          <div>
+            <h1 className='text-center text-purple-900 text-2xl
+          font-semibold'>Sign In</h1>
+          </div>
+          <br /><br /><br />
+          <div>
+            <input className='
+          bg-gray-200 w-64 mx-12 outline-none p-1
+          rounded-xl text-center font-semibold placeholder-purple-600'
+              placeholder='Username' type="text" name="" id="" />
+            <br /><br />
+            <input className='
+          bg-gray-200 w-64 mx-12 outline-none p-1
+          rounded-xl placeholder-purple-600 text-center font-semibold'
+              placeholder='Password' type="text" name="" id="" />
+          </div>
+          <br /><br />
+          <div className='flex justify-center'>
+            <button className='p-2 w-32 rounded-2xl bg-purple-600
+           text-white'>Sign In</button>
+          </div>
+          <br />
+          <div>
+            <p className='text-center text-purple-500
+          '>Forgot Password?</p>
+          </div>
+          <br />
+        </div>
+      </div>
+    </ThemeContext.Provider>
+  </>
+)
 }
 
 export default App
