6161e7b87fb792047e256b821477bbdf8aac8af4
mohammed kaif <mkaif555123@gmail.com>
2024-10-19 15:12:25 +0500

    80% Work Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { createContext } from 'react'
-import './App.css'
 import { useState } from 'react';
+import { createContext } from 'react'
+import Form from './components/Form';
+import './App.css'
 
 
 export const ThemeContext = createContext(null);
-
 
 function App() {
 
@@ -16,46 +16,15 @@ function App() {
     });
   };
 
-
-return (
-  <>
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
-
-      <div id='light'>
-        <div className='p-4 bg-white w-96 mx-auto my-28 rounded-xl'>
-          <br />
-          <div>
-            <h1 className='text-center text-purple-900 text-2xl
-          font-semibold'>Sign In</h1>
-          </div>
-          <br /><br /><br />
-          <div>
-            <input className='
-          bg-gray-200 w-64 mx-12 outline-none p-1
-          rounded-xl text-center font-semibold placeholder-purple-600'
-              placeholder='Username' type="text" name="" id="" />
-            <br /><br />
-            <input className='
-          bg-gray-200 w-64 mx-12 outline-none p-1
-          rounded-xl placeholder-purple-600 text-center font-semibold'
-              placeholder='Password' type="text" name="" id="" />
-          </div>
-          <br /><br />
-          <div className='flex justify-center'>
-            <button className='p-2 w-32 rounded-2xl bg-purple-600
-           text-white'>Sign In</button>
-          </div>
-          <br />
-          <div>
-            <p className='text-center text-purple-500
-          '>Forgot Password?</p>
-          </div>
-          <br />
+  return (
+    <>
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+        <div className='app' id={theme}>
+          <Form />
         </div>
-      </div>
-    </ThemeContext.Provider>
-  </>
-)
+      </ThemeContext.Provider >
+    </>
+  )
 }
 
 export default App
